d3f3ac96a149b86992dfcc610d359a8b34c2944d
Aleksa Bubanja <aleksa3b@gmail.com>
2021-11-29 18:26:57 +0100

    useFetch hook finished

---
+++ src/useFetch.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+const apiLink = "https://www.breakingbadapi.com/api/";
+
+const useFetch = (urlParams) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const fetchData = async (url) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setData(data);
+      //   if (data.Response === "True") {
+      //   }
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(`${apiLink}${urlParams}`);
+  }, [urlParams]);
+
+  return { isLoading, data };
+};
+
+export default useFetch;
